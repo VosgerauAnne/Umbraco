7975abfdcb0af56e672142c0127cb39112e71764
LAPTOP-83B6VDAS\vosge <eaaanvos@students.eaaa.dk>
2018-03-26 15:20:12 +0200

    master

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Core.Models;
+using AarhusWebDevCoop.ViewModels;
+using System.Net.Mail;
+
+namespace AarhusWebDevCoop.Controllers
+
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model) {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            TempData["success"] = true;
+
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "message");
+            comment.SetValue("messageName", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("messageContent", model.Message);
+            comment.SetValue("umbracoNaviHide", true);
+
+            // Save
+            Services.ContentService.Save(comment);
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
